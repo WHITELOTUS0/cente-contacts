4cc1741e1691df26b46fde7f0ae56c3e00e8af49
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-07-31 15:59:28 +0300

    updates on sticky bottom

--- src/StickyStrip.js
+++ src/StickyStrip.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './StickyStrip.css'; // Import the CSS file for styling
 
 const StickyStrip = () => {
+    const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    // Use a timeout to show the sticky strip after a short delay (e.g., 1 second)
+    const timer = setTimeout(() => {
+      setShow(true);
+    }, 1000);
+
+    // Clean up the timeout when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
     return (
-        <div className="sticky-strip">
+        <div className={`sticky-strip ${show ? 'show' : ''}`}>
             {/* Section 1 (Blue) */}
             <div className="section blue">
                 <h4>CENTE-TECH 2023</h4>
