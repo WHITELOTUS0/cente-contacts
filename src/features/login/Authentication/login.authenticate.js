1ac2fa25217b7e35672bcc9fcd7ecd8af3412061
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-08-01 14:16:00 +0300

    Update login.authenticate.js

--- src/features/login/Authentication/login.authenticate.js
+++ src/features/login/Authentication/login.authenticate.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
 import "./login.authenticate.css";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+
+const loginUser= async(credentials)=>{
+    return fetch('http://localhost:8080/login', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
+}
+
 
 const LoginAuthenticate=({setToken})=>{
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
-    
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+          username,
+          password
+        });
+        setToken(token);
+      }
+
     return(
         <div className="login-wrapper">
             <h1>Log in</h1>
         <form>
       <label>
         <p>Username</p>
-        <input type="text" />
+        <input type="text" onChange={e => setUserName(e.target.value)}/>
       </label>
       <label>
         <p>Password</p>
-        <input type="password" />
+        <input type="password" onChange={e => setPassword(e.target.value)}/>
       </label>
       <div>
         <button type="submit">Submit</button>
