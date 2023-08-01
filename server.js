497a2457097ef73cea035f84c08c9d10db37c62f
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-08-01 14:06:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors())
+
+app.use('/login', (req,res)=>{
+    res.send({
+        token: 'test123'
+    });
+})
+
+app.listen(3600, ()=>console.log("Server is listening at port 3600"))
