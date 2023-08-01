336045b581a88fa87e57d5587a54d6f247649835
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-08-01 10:32:34 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './features/home/screen/home';
+import Home from './features/home/screen/home.screen';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
